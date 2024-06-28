caff2df56fc7c3574be9eafd13bb0c64b86665eb
Chris Carter <cjcsweatbox@outlook.com>
2024-06-28 19:22:51 +0100

    Added console project

---
+++ src/HabitTracker/HabitTracker.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HabitTracker.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
